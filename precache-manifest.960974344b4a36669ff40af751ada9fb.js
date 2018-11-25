1b21220d00b82aae9f4ffc9bf576be3fea583880
dunjamarc <dunjamarkovic86@gmail.com>
2018-11-25 16:44:00 +0100

    Updates

--- precache-manifest.960974344b4a36669ff40af751ada9fb.js
+++ precache-manifest.960974344b4a36669ff40af751ada9fb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/memory-game/static/js/runtime~main.66c34b29.js"
   },
   {
-    "revision": "181070489be3d489d47b",
-    "url": "/memory-game/static/js/main.18107048.chunk.js"
+    "revision": "84b7fc88693298f69bdf",
+    "url": "/memory-game/static/js/main.84b7fc88.chunk.js"
   },
   {
     "revision": "37f90d57916b981a0157",
     "url": "/memory-game/static/js/1.37f90d57.chunk.js"
   },
   {
-    "revision": "181070489be3d489d47b",
-    "url": "/memory-game/static/css/main.9f1144a4.chunk.css"
+    "revision": "84b7fc88693298f69bdf",
+    "url": "/memory-game/static/css/main.258a9b27.chunk.css"
   },
   {
-    "revision": "707238a733b731638b06979a786e464f",
+    "revision": "7af329a6d0051d058c67130ad983ca76",
     "url": "/memory-game/index.html"
   }
 ];
